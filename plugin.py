3f36828e8e2e37a4998903ba2b48b5b85eac2b00
Clint Savage <herlo1@gmail.com>
2011-07-01 07:39:43 -0600

    initial python code for supybot-irc2rss

---
+++ plugin.py
@@ -0,0 +1,120 @@
+###
+# Copyright (c) 2011, Clint Savage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+import os
+import time
+import string
+import datetime
+
+import PyRSS2Gen
+
+import supybot.conf as conf
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class Irc2rss(callbacks.Plugin):
+    """Add the help for "@plugin help Irc2rss" here
+    This should describe *how* to use this plugin."""
+
+    def __init__(self, irc):
+        self.__parent = super(Irc2rss, self)
+        self.__parent.__init__(irc)
+        self.path = self.registryValue('basepath' )
+
+    def _generateFeed(self):
+        # generate the rss.xml
+
+        self.rsstitle = self.registryValue('title') % {'user': self.nick}
+        self.rsslink = self.registryValue('link') % {'user': self.nick}
+        self.rssdesc = self.registryValue('description')
+
+        f = open("%s/%s/rss.in" % (self.path, self.nick), 'r')
+
+        item_list = []
+        for line in f:
+            t, u, d, dt = line.split("|")
+#            time.sleep(2)
+
+            print "t: %s, u: %s, dt: %s, d: %s" % (t, u, dt, d)
+            item = PyRSS2Gen.RSSItem(title = t, link = u, description = d, guid = PyRSS2Gen.Guid(u + dt), pubDate = dt)
+            item_list.append(item)
+
+        f.close()
+ 
+        rss = PyRSS2Gen.RSS2( title = self.rsstitle, link = self.rsslink, description = self.rssdesc, lastBuildDate = self.pubdate, items = item_list)        
+        file("%s/%s/rss.xml" % (self.path, self.nick), 'w+').write(rss.to_xml())
+
+    def generateRss(self, irc, msg, args, text):
+
+        self.nick = msg.nick
+        #self.path = conf.supybot.plugins.Irc2rss.basepath()
+        irc.reply("generating feed for: %s" % self.nick)
+        self._generateFeed()
+
+    generateRss = wrap(generateRss, [many('something')])
+
+    def add2rss(self, irc, msg, args, text):
+
+        self.nick = msg.nick
+        dateformat = self.registryValue('dateFormat')
+        d = datetime.datetime.now()
+        self.pubdate = d.strftime(dateformat)
+
+        if not os.path.exists(u"%s/%s/rss.in" % (self.path, self.nick)):
+            if not os.path.isdir(u"%s/%s" % (self.path, nick)):
+                os.makedirs(u"%s/%s" % (self.path, self.nick), 0775)
+#            f = open("%s/%s/rss.in" % (self.path, nick), 'a')
+#            f.write("==%s|%s|%s|%s|==\n" % (self.rsstitle, nick, self.rsslink, self.rssdesc)) 
+
+        f = open("%s/%s/rss.in" % (self.path, self.nick), 'a')
+
+        itemmessage = string.join(text)
+        itemlink = self.registryValue('link') % {'user': self.nick}
+        itemdesc = "%s's status on %s" % (self.nick, self.pubdate)
+
+        f.write("%s|%s|%s|%s\n" % (itemmessage, itemlink, itemdesc, self.pubdate))
+
+        irc.reply("message: %s" % itemmessage)
+
+        f.close()
+
+        irc.reply("generating feed for: %s" % self.nick)
+        self._generateFeed()
+
+    add2rss = wrap(add2rss, [many('something')])
+
+
+Class = Irc2rss
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
